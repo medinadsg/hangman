9f0a515233bef29ac80d4a202cc59a3cb90bdff9
Adina <178459927+medinadsg@users.noreply.github.com>
2024-08-15 17:55:53 +0000

    handle keyboard input and refactor

--- script.js
+++ script.js
@@ -1,8 +1,90 @@
-const wordEl = document.getElementById('word');
-const keyboardEl = document.getElementById('keyboard');
-const guessesLeftEl = document.getElementById('guesses');
-const hangmanImageEl = document.getElementById('hangman-image');
-const resetButton = document.getElementById('reset');
+class HangmanGame {
+  constructor(wordList, wordEl, keyboardEl, guessesLeftEl, resetButton) {
+    this.wordList = wordList;
+    this.wordEl = wordEl;
+    this.keyboardEl = keyboardEl;
+    this.guessesLeftEl = guessesLeftEl;
+    this.resetButton = resetButton;
+    this.word = '';
+    this.guessesLeft = 6;
+    this.guessedLetters = [];
+  }
+
+  chooseWord() {
+    this.word = this.wordList[Math.floor(Math.random() * this.wordList.length)];
+  }
+
+  updateGuessesLeft() {
+    this.guessesLeftEl.textContent = this.guessesLeft;
+  }
+
+  updateWordDisplay() {
+    const displayWord = this.word.split('').map(letter => this.guessedLetters.includes(letter) ? letter : '_').join(' ');
+    this.wordEl.textContent = displayWord;
+  }
+
+  createKeyboard() {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    for (let i = 0; i < alphabet.length; i++) {
+      const button = document.createElement('button');
+      button.textContent = alphabet[i];
+      button.addEventListener('click', () => this.handleLetterClick(alphabet[i]));
+      this.keyboardEl.appendChild(button);
+    }
+  }
+
+  updateHangmanImage() {}
+
+  resetGame() {
+    this.wordEl.textContent = '';
+    this.keyboardEl.innerHTML = '';
+    this.guessesLeft = 6;
+    this.guessedLetters = [];
+    this.chooseWord();
+    this.updateHangmanImage();
+    this.updateGuessesLeft();
+    this.createKeyboard();
+  }
+
+  handleLetterClick(letter) {
+    if (this.guessedLetters.includes(letter)) {
+      return;
+    }
+
+    this.guessedLetters.push(letter);
+
+    if (!this.word.includes(letter)) {
+      this.guessesLeft--;
+    }
+
+    this.updateWordDisplay();
+    this.updateHangmanImage();
+    this.updateGuessesLeft();
+
+    if (this.word.split('').every(l => this.guessedLetters.includes(l))) {
+      alert('You win!');
+      this.resetGame();
+    } else if (this.guessesLeft === 0) {
+      alert('You lose! The word was ' + this.word);
+      this.resetGame();
+    }
+  }
+
+  startGame() {
+    this.chooseWord();
+    this.updateWordDisplay();
+    this.updateHangmanImage();
+    this.updateGuessesLeft();
+    this.createKeyboard();
+    this.resetButton.addEventListener('click', () => this.resetGame());
+    document.addEventListener('keydown', (e) => {
+      const letter = e.key.toLowerCase();
+      if (letter.match(/[a-z]/) && !this.guessedLetters.includes(letter)) {
+        this.handleLetterClick(letter);
+      }
+    });
+  }
+}
 
 const wordList = [
   'javascript', 
@@ -12,74 +94,10 @@ const wordList = [
   'python'
 ];
 
-let word = '';
-let guessesLeft = 6;
-let guessedLetters = [];
+const wordEl = document.getElementById('word');
+const keyboardEl = document.getElementById('keyboard');
+const guessesLeftEl = document.getElementById('guesses');
+const resetButton = document.getElementById('reset');
 
-function chooseWord() {
-  word = wordList[Math.floor(Math.random() * wordList.length)];
-}
-
-function updateGuessesLeft() {
-  guessesLeftEl.textContent = guessesLeft;
-}
-
-function updateWordDisplay() {
-  const displayWord = word.split('').map(letter => guessedLetters.includes(letter) ? letter : '_').join(' ');
-  wordEl.textContent = displayWord;
-}
-
-function createKeyboard() {
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  for (let i = 0; i < alphabet.length; i++) {
-    const button = document.createElement('button');
-    button.textContent = alphabet[i];
-    button.addEventListener('click', () => handleLetterClick(alphabet[i]));
-    keyboardEl.appendChild(button);
-  }
-}
-
-function updateHangmanImage() {}
-
-function resetGame() {
-  wordEl.textContent = '';
-  keyboardEl.innerHTML = '';
-  guessesLeft = 6;
-  guessedLetters = [];
-  chooseWord();
-  updateHangmanImage();
-  updateGuessesLeft();
-  createKeyboard();
-}
-
-chooseWord();
-updateWordDisplay();
-updateHangmanImage();
-updateGuessesLeft();
-createKeyboard();
-
-resetButton.addEventListener('click', resetGame);
-
-function handleLetterClick(letter) {
-  if (guessedLetters.includes(letter)) {
-    return;
-  }
-
-  guessedLetters.push(letter);
-
-  if (!word.includes(letter)) {
-    guessesLeft--;
-  }
-
-  updateWordDisplay();
-  updateHangmanImage();
-  updateGuessesLeft();
-
-  if (word.split('').every(l => guessedLetters.includes(l))) {
-    alert('You win!');
-    resetGame();
-  } else if (guessesLeft === 0) {
-    alert('You lose! The word was ' + word);
-    resetGame();
-  }
-}
+const game = new HangmanGame(wordList, wordEl, keyboardEl, guessesLeftEl, resetButton);
+game.startGame();
